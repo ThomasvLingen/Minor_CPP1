9a7beb68b04afa6bbbac8a558956af15fda952b9
ThomasvLingen <thovali96@gmail.com>
2016-10-21 10:42:46 +0200

    docs: add function docs to EnemyFactory

--- src/monsters/EnemyFactory.cpp
+++ src/monsters/EnemyFactory.cpp
@@ -4,12 +4,16 @@
 
 #include "EnemyFactory.hpp"
 
-namespace Monsters {
+namespace Monsters
+{
     EnemyFactory::EnemyFactory(string monsters_file_name)
     {
         this->_known_monsters = MonsterFileParser::parse_monsters_file(monsters_file_name);
     }
 
+    /// \brief This function returns an enemy object to copy, the caller has to copy the enemy object
+    /// \param name Name of the enemy to copy
+    /// \return Enemy to copy
     Enemy EnemyFactory::create_enemy(string name)
     {
         return this->_known_monsters.at(name);
