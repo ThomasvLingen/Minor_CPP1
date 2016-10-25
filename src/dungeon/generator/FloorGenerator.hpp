68ca5e36a941d86c99ba3c2ab919ddfceef5ef63
jorgdebont <jacbont@student.avans.nl>
2016-10-25 18:22:39 +0200

    Removed algorithms for floorgeneration out of floorgenerator

--- src/dungeon/generator/FloorGenerator.hpp
+++ src/dungeon/generator/FloorGenerator.hpp
@@ -5,30 +5,16 @@
 #ifndef MINOR_CPP1_FLOORGENERATOR_HPP
 #define MINOR_CPP1_FLOORGENERATOR_HPP
 
-
-#include <dungeon/Floor.hpp>
-#include <set>
+#include "dungeon/Floor.hpp"
+#include "util/RandomUtil.hpp"
+#include "FloorPlan.hpp"
 
 class FloorGenerator {
 public:
     FloorGenerator();
     virtual ~FloorGenerator();
 
-    static Floor* generateFloor(std::size_t height, std::size_t width);
-
-private:
-    static Room::Location set_start_and_end_room(Floor *floor, std::size_t height, std::size_t width);
-    static int get_random_room_index(std::size_t width);
-    static std::vector<Room::Location> create_neighbours(Floor *floor, std::vector<Room::Location> room_list);
-    static void set_room_in_floor(Floor *floor, Room *room);
-    static std::vector<Room::Location> get_open_neighbours(Floor *floor, Room::Location location);
-    static std::vector<Room::Location> get_raw_neighbours(Room::Location location); //does not check if valid
-    static bool is_available(Floor *floor, Room::Location location);
-    static std::set<int> get_random_ints_in_range(int max_range);
-    static std::vector<int> get_random_gen_weights(int range);
-    static int get_weighted_int(int range);
-
+    Floor* generate_floor(std::size_t height, std::size_t width);
 };
-
 
 #endif //MINOR_CPP1_FLOORGENERATOR_HPP
